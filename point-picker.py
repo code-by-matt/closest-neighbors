f48e351d055ad85387edaf834ad6f270f7bb233e
Matt Yuan <matthewfelixyuan@gmail.com>
2019-12-06 16:57:23 -0500

    let point-picker.py save to test-sets.txt

--- point-picker.py
+++ point-picker.py
@@ -3,4 +3,9 @@ import sys
 
 ax = plt.gca().set_aspect('equal', 'datalim')
 points = plt.ginput(int(sys.argv[1]), show_clicks=True)
-print(points)
+
+save = input('Type J to save points to test-sets.txt: ')
+if save == 'J':
+    txt_file = open('test-sets.txt', 'a')
+    txt_file.write(str(points))
+    txt_file.close()
