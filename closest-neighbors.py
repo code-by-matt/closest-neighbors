435b7c8cc99fd8c7f064ab5cb98ea8ee482af71f
Matt Yuan <matthewfelixyuan@gmail.com>
2019-12-07 02:18:39 -0500

    code overhaul using new SortedPoints class

--- closest-neighbors.py
+++ closest-neighbors.py
@@ -8,40 +8,53 @@ class SortedPoints:
         self.n1 = n1
         self.n2 = n2
 
-# input is a list of ordered pairs, output is a list of
+
+# input is a SortedPoints object, output is a list of two SortedPoints objects
+def split(sorted_points):
+    # the vertical line x = thresh splits the points in half
+    # (assuming no two points have the same x-coordinate)
+    mid = sorted_points.size // 2
+    thresh = (sorted_points.x_sort[mid][0] + sorted_points.x_sort[mid+1][0]) / 2
+    # create the horizontal half orderings
+    lx_sort = []
+    rx_sort = []
+    for p in sorted_points.x_sort:
+        if p[0] < thresh: lx_sort.append(p)
+        else: rx_sort.append(p)
+    # create the vertical half orderings
+    ly_sort = []
+    ry_sort = []
+    for p in sorted_points.y_sort:
+        if p[0] < thresh: ly_sort.append(p)
+        else: ry_sort.append(p)
+    # return a list of two SotredPoints objects
+    return [SortedPoints(lx_sort, ly_sort, None, None), SortedPoints(rx_sort, ry_sort, None, None)]
+
+
+# input is two SortedPoints objects, output is a single SortedPoints object
+def merge(left_points, right_points):
+    # some thought is needed here
+    return merged_points
+
+
+# input is a list of points (ordered pairs), output is a list of
 # the two ordered pairs that are the closest neighbors
-def closest_neighbors(points):
+def closest_neighbors(points_list):
 
-    x_sorted_points = sorted(points, key=lambda p: p[0])
-    y_sorted_points = sorted(points, key=lambda p: p[1])
-
-    # n1, n2 are the closest neighbors among x_sorted_points[lo:hi]
-    class PointRange:
-        def __init__(self, lo, hi, n1, n2):
-            self.lo = lo
-            self.hi = hi
-            self.n1 = n1
-            self.n2 = n2
-
-    # output is a PointRange object corresponding to x_sorted_points[lo:hi]
-    def recur(lo, hi):
-
+    # input is a SortedPoints object with unknown closest neighbors,
+    # output is a SortedPoints object but with closest neighbors identified
+    def recur(sorted_points):
         # recursion ends
-        if hi == lo + 1:
-            return PointRange(lo, hi, None, None)
-
+        if sorted_points.size == 1:
+            return sorted_points
         # recur on half-ranges
         else:
-            mid = (lo + hi) // 2  # always strictly between lo and hi
-            left_range = recur(lo, mid)
-            right_range = recur(mid, hi)
-            return merge(left_range, right_range)
+            [left_points, right_points] = split(sorted_points)
+            return merge(left_points, right_points)
 
-    def merge(left_range, right_range):
+    x_sort = sorted(points_list, key=lambda p: p[0])
+    y_sort = sorted(points_list, key=lambda p: p[1])
+    sorted_points = SortedPoints(x_sort, y_sort, None, None)
+    sorted_points = recur(sorted_points)
 
-        # more thought is needed here
-
-        return merged_range
-
-    full_range = recur(0, len(points) - 1)
-    return [x_sorted_points[full_range.n1], x_sorted_points[full_range.n2]]
+    return [sorted_points.n1, sorted_points.n2]
